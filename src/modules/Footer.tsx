82499536f2989e75d628a4b5ab88a75a065c38f9
Shreya Reshamwala <shreyar@mit.edu>
2023-05-16 16:29:34 -0400

    revised about me and footer CSS

--- src/modules/Footer.tsx
+++ src/modules/Footer.tsx
@@ -13,14 +13,14 @@ const Footer = () => {
           <li className='u-inlineBlock'>
             <div className="imgResize">
               <a href="https://github.com/shreyaresh" target="_blank" rel="noreferrer">
-                <img className='Image' src={'https://cdn-icons-png.flaticon.com/512/25/25231.png'} alt="Github Link"></img>
+                <img className='Image u-link' src={'https://cdn-icons-png.flaticon.com/512/25/25231.png'} alt="Github Link"></img>
               </a>
             </div>
           </li>
           <li className='u-inlineBlock'>
             <div className="imgResize">
               <a href="https://linkedin.com/in/shreyareshamwala/" target="_blank" rel="noreferrer">
-                <img className='Image' src={'https://www.iconpacks.net/icons/1/free-linkedin-icon-112-thumb.png'} alt="LinkedIn Link"></img>
+                <img className='Image u-link' src={'https://www.iconpacks.net/icons/1/free-linkedin-icon-112-thumb.png'} alt="LinkedIn Link"></img>
               </a>
             </div>
      
@@ -28,7 +28,7 @@ const Footer = () => {
           <li className='u-inlineBlock'>
             <div className="imgResize">
               <a href="mailto:shreyar@mit.edu" target="_blank" rel="noreferrer">
-                <img className='Image' src={'https://www.freeiconspng.com/thumbs/email-icon/email-icon--clipart-best-22.png'} alt="Email"></img>
+                <img className='Image u-link' src={'https://www.freeiconspng.com/thumbs/email-icon/email-icon--clipart-best-22.png'} alt="Email"></img>
               </a>
             </div>
           </li>
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,9 +6,9 @@ import "./AboutMe.css";
 
 
 const name_header : string = 'My name is Shreya Reshamwala.';
-const aboutMep1 : string = `I am a third-year student at the Massachusetts Institute of Technology. I am a double major--my primary major is electrical engineering and computer science, and my secondary major is pure mathematics. I also have a minor in German.`;
-const aboutMep2: string = `I love to create and build. Software engineering and pure math in particular offer a lot of room for creativity in problem solving, and so I find myself enjoying my CS project and math classes the most. I’m driven by interesting problems and breaking the comfort zone, and so I like to pursue a variety of projects in different fields and learn new things along the way.`;
-const aboutMep3: string = `My programming language of preference is Python; however, I am comfortable coding in C as well. I’m also familiar with JavaScript/TypeScript, SQL, and HTML/CSS.`;
+const aboutMep1 : string = `I am a third-year student at the Massachusetts Institute of Technology. I'm majoring in computer science, and I have a minor in German and math.`;
+const aboutMep2: string = `I love to create and build. Software engineering and math offer a lot of room for creativity in problem solving, and so I find myself enjoying my CS project and math classes the most. I’m driven by interesting problems and breaking the comfort zone, and so I like to pursue a variety of projects in different fields and learn new things along the way.`;
+const aboutMep3: string = `My programming language of preference is Python and JavaScript/TypeScript; however, I am comfortable coding in C as well. I’m also familiar with SQL and HTML/CSS.`;
 const aboutMep4: string = `Outside of academics, I love following foreign policy, appreciating art (especially German abstract expressionism and Bauhaus, from which this site is inspired by), Lord of the Rings, spontaneity, and music.`;
 
 document.body.style.backgroundColor = "#FFFCEF";
